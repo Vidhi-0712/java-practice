3e5ad899689a223fcd169db8ff6726c16707576f
Vidhisha <vidhishapatil777@gmail.com>
2025-07-24 10:00:03 +0530

    ArraySum Program

--- ArraySum.java
+++ ArraySum.java
@@ -4,6 +4,7 @@ public class ArraySum {
         float [] abc= {1.2f, 1.3f, 1.4f, 1.5f};
         float sum=0.0f;
 
+
         for(int i=0; i<=abc.length-1; i++){
             sum=sum+abc[i];
         }
